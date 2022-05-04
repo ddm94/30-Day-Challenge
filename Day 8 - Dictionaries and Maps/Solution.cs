c0f0f1b4c83a0fa5b9b1ca798303035401d77327
Daniele Di Modica <d.dimo94@hotmail.com>
2022-05-04 18:44:48 +0100

    Added Day 8

---
+++ Day 8 - Dictionaries and Maps/Solution.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution {
+    static void Main(String[] args) {
+        /* Enter your code here. Read input from STDIN. 
+        Print output to STDOUT. Your class should be named Solution */
+        int n = Convert.ToInt32(Console.ReadLine());
+        
+        Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+        
+        for (int i = 0; i < n; i++) 
+        {   
+            string[] entry = Console.ReadLine().Split(' ');
+            
+            phoneBook.Add(entry[0], entry[1]);
+        }
+        
+        string name;
+        
+        while ((name = Console.ReadLine()) != null)
+        {
+            if (phoneBook.ContainsKey(name))
+            {
+                Console.WriteLine(name + "=" + phoneBook[name]);
+            }
+            else
+            {
+                Console.WriteLine("Not found");
+            }
+        }               
+    }
+}
