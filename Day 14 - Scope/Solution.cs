4512c180ea5a4918446633bbff6355d9b45806b3
Daniele Di Modica <d.dimo94@hotmail.com>
2022-05-10 18:37:47 +0100

    Added Day 14

---
+++ Day 14 - Scope/Solution.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Linq;
+
+class Difference 
+{
+	private int[] elements;
+    public int maximumDifference;
+
+	// Add your code here
+    public Difference(int[] elements)
+    {
+        this.elements = elements;
+    }
+    
+    public void computeDifference()
+    {
+        for(int i = 0; i < elements.Length; i++)
+        {
+            for (int j = 0; j < elements.Length; j++) 
+            {
+                int tempDifference = 0;
+                
+                tempDifference = Math.Abs(elements[i] - elements[j]);
+                
+                if (tempDifference > maximumDifference)
+                {
+                    maximumDifference = tempDifference;
+                }
+            }
+        }
+    }
+} // End of Difference Class
+
+class Solution 
+{
+    static void Main(string[] args) 
+	{
+        Convert.ToInt32(Console.ReadLine());
+        
+        int[] a = Console.ReadLine().Split(' ').Select(x=>Convert.ToInt32(x)).ToArray();
+        
+        Difference d = new Difference(a);
+        
+        d.computeDifference();
+        
+        Console.Write(d.maximumDifference);
+    }
+}
